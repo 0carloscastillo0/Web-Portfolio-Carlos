cdef934883ed805fde383efd6ef284d9cba100a9
0carloscastillo0 <carlos.castillo.d@usach.cl>
2026-04-09 00:22:42 -0400

    Add: install coverage-v8 in frontend project

--- frontend-client/vite.config.ts
+++ frontend-client/vite.config.ts
@@ -15,6 +15,10 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html", "lcov"],
+    },
   },
   resolve: {
     alias: {
